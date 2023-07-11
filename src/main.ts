710a1aeecf3da8cc3e8d9a5a15e9de8ee39dfd9d
leonardo_marchioro <leonardomarchioro11@gmail.com>
2023-07-11 20:10:07 -0300

    fix: move handlers to correct past

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './global/modules/app.module';
 
 import { config } from 'dotenv';
 import { useContainer } from 'class-validator';
-import { setupPrisma } from './global/helpers/prisma.handler';
-import { setupGlobalPipes } from './global/helpers/global-pipes.handler';
+import { setupPrisma } from './global/handlers/prisma.handler';
+import { setupGlobalPipes } from './global/handlers/global-pipes.handler';
 
 
 async function bootstrap() {
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true })
 
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 4000
 
   await setupPrisma({ app })
   await setupGlobalPipes({ app })
