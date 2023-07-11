a01e9ae1d1b4badd237c96bf2f61fad0e1a58241
leonardo_marchioro <leonardomarchioro11@gmail.com>
2023-07-11 18:13:30 -0300

    feat: setup prisma and first table 'users'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './global/modules/app.module';
+
+import { config } from 'dotenv';
+import { useContainer } from 'class-validator';
+import { setupPrisma } from './global/helpers/prisma.handler';
+import { setupGlobalPipes } from './global/helpers/global-pipes.handler';
+
 
 async function bootstrap() {
+  config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true })
+
+  const port = process.env.PORT || 3000
+
+  await setupPrisma({ app })
+  await setupGlobalPipes({ app })
+
+
+  await app.listen(port);
+
+  console.log(`App is running on: http://localhost:${port}`)
 }
 bootstrap();
